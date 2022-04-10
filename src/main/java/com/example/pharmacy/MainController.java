e4aa0fd9e5d2c0e43a53b43b3c6900a3e8ffe9db
Mostafa Amer <mamer3897@gmail.com>
2022-04-11 01:33:03 +0200

    mostafa Amer file finishing GUI

--- src/main/java/com/example/pharmacy/MainController.java
+++ src/main/java/com/example/pharmacy/MainController.java
@@ -1,13 +1,10 @@
 package com.example.pharmacy;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
 public class MainController {
-
 
     @FXML
     private BorderPane mainPane;
@@ -44,6 +41,48 @@ public class MainController {
         try {
             FxmlLoader loader = new FxmlLoader();
             Pane view = loader.getView("SalesReview.fxml");
+            mainPane.setCenter(view);
+            System.out.println("success");
+
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+
+    @FXML
+    public void  handleLoadAddUserScreen(ActionEvent event){
+        try {
+
+            FxmlLoader loader = new FxmlLoader();
+            Pane view = loader.getView("AddUser.fxml");
+            mainPane.setCenter(view);
+            System.out.println("success");
+
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+
+    @FXML
+    public void  handleLoadUserReviewScreen(ActionEvent event){
+        try {
+
+            FxmlLoader loader = new FxmlLoader();
+            Pane view = loader.getView("UserReview.fxml");
+            mainPane.setCenter(view);
+            System.out.println("success");
+
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+
+    @FXML
+    public void  handleLoadUpdateDeleteScreen(ActionEvent event){
+        try {
+
+            FxmlLoader loader = new FxmlLoader();
+            Pane view = loader.getView("UpdateDelete.fxml");
             mainPane.setCenter(view);
             System.out.println("success");
 
