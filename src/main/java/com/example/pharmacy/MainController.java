c2b8bcd160573d991f5d640174a25ea2e0dfdd76
Mostafa Amer <mamer3897@gmail.com>
2022-04-13 17:41:53 +0200

    fix bugs

--- src/main/java/com/example/pharmacy/MainController.java
+++ src/main/java/com/example/pharmacy/MainController.java
@@ -6,7 +6,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
-public class HelloController {
+public class MainController {
 
 
     @FXML
